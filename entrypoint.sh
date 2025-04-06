c3f88f08d306410ccd4eb09401b4c033a3f50acb
Teyllay <104153795+Teyllayka@users.noreply.github.com>
2025-04-06 03:58:49 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Создание файлов перевода и их компиляция
 django-admin makemessages -l ru
 django-admin compilemessages
+python manage.py collectstatic --noinput
 
 # Выполнение основной команды запуска
 exec "$@"
